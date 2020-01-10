23d830a6f785cdfc1013060ba3f7ca0233db21b3
upieez <samuelhww@gmail.com>
2020-01-10 09:08:05 +0800

    done

--- hexToRgb.js
+++ hexToRgb.js
@@ -1,4 +1,4 @@
-var hexToRgb = function(hex) {
+const hexToRgb = function(hex) {
   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
   let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const hexToRgb = require('./hexToRgb')
-const rgbToHex = require('./rgbToHex')
+const hexToRgb = require('./hexToRgb');
+const rgbToHex = require('./rgbToHex');
+const rgbToHsl = require('./rgbToHsl');
+const hexToHsl = require('./hexToHsl');
+const r = parseInt((process.argv[3]));
+const g = parseInt((process.argv[4]));
+const b = parseInt((process.argv[5]));
 
-// console.log(rgbToHex(0, 51, 255)); // #0033ff
 
 if(process.argv[2] === "hex"){
-    console.log(hexToRgb(process.argv[3]));
+    for(let i = 3; i < process.argv.length; i++){
+    console.log(hexToRgb(process.argv[i]));
+    console.log(hexToHsl(process.argv[i]));
+    }
 } else if(process.argv[2] === "rgb"){
-    console.log(rgbToHex(
-            parseInt((process.argv[3])),
-            parseInt((process.argv[4])),
-            parseInt((process.argv[5]))
-        )
-    );
+    console.log(rgbToHex(r,g,b));
+    console.log(rgbToHsl(r,g,b));
+} else if(process.argv[2] === "color"){
+    myOutput = process.argv[6];
+    console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
 }
